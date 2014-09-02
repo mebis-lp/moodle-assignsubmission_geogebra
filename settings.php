1c0560b92ac444af5b0487d58b0dd6aa05a4f129
christoph.stadlbauer <christoph.stadlbauer@872ce066-4876-4fca-a6fc-23b94bf576d1>
2014-09-02 10:02:21 +0000

    New Directory structure
    git-svn-id: https://dev.geogebra.org/internal/moodle/moodle-assignsubmission_geogebra/trunk@3014 872ce066-4876-4fca-a6fc-23b94bf576d1

---
+++ settings.php
@@ -0,0 +1,31 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * This file defines the admin settings for this plugin
+ *
+ * @package        assignsubmission_onlinetext
+ * @author         Christoph Stadlbauer <christoph.stadlbauer@geogebra.org>
+ * @copyright  (c) International GeoGebra Institute 2014
+ * @license        http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later,
+ * license of GeoGebra: http://creativecommons.org/licenses/by-nc-nd/3.0/
+ * For commercial use please see: http://www.geogebra.org/license
+ */
+
+$settings->add(new admin_setting_configcheckbox('assignsubmission_geogebra/default',
+        new lang_string('default', 'assignsubmission_geogebra'),
+        new lang_string('default_help', 'assignsubmission_geogebra'), 0));
+
