171f097ce53cacf464ac8e358e8fad1afffab69f
christoph.stadlbauer <christoph.stadlbauer@872ce066-4876-4fca-a6fc-23b94bf576d1>
2014-09-02 09:59:08 +0000

    New Directory structure
    git-svn-id: https://dev.geogebra.org/internal/moodle/moodle-assignsubmission_geogebra/trunk@3009 872ce066-4876-4fca-a6fc-23b94bf576d1

---
+++ lang/de/assignsubmission_geogebra.php
@@ -0,0 +1,48 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'assignsubmission_geogebra', language 'de'
+ *
+ * @package        assignsubmission_geogebra
+ * @author         Christoph Stadlbauer <christoph.stadlbauer@geogebra.org>
+ * @copyright  (c) International GeoGebra Institute 2014
+ * @license        http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later,
+ * license of GeoGebra: http://creativecommons.org/licenses/by-nc-nd/3.0/
+ * For commercial use please see: http://www.geogebra.org/license
+ */
+
+$string['allowgeogebrasubmissions'] = 'Aktiviert';
+$string['default'] = 'Standardmäßig aktiviert';
+$string['default_help'] = 'Die gewählte Methode für die Abgabe von Lösungen wird für alle neuen Aufgaben voreingestellt.';
+$string['enabled'] = 'GeoGebra';
+$string['enabled_help'] = 'Diese Methode ermöglicht den Teilnehmer/innen die Aufgabe direkt in GeoGebra zu bearbeiten.';
+$string['geogebra'] = 'GeoGebra';
+$string['geogebrafilename'] = 'geogebra.html';
+$string['geogebrasubmission'] = 'GeoGebra Abgaben erlauben';
+$string['pluginname'] = 'GeoGebra Abgabetyp';
+$string['ggbturl'] = 'GeoGebravorlage für den Teilnehmer';
+$string['ggbturl_help'] = 'Die Perspektiven funktionieren zur Zeit noch nicht perfekt, da für diese GeoGebra 5.0 Beta verwendet
+wird. Benutzerdefinierte Vorlagen von GeoGebraTube funktionieren problemlos.';
+$string['ggbtemplates'] = 'Vorlage wählen';
+$string['algebragraphics'] = 'Algebra und Grafik';
+$string['basicgeo'] = 'Elementare Geometrie';
+$string['geometry'] = 'Geometrie';
+$string['spreadsheetgraphics'] = 'Tabelle und Grafik';
+$string['casgraphics'] = 'CAS und Grafik';
+$string['graphics3d'] = '3D Grafik';
+$string['userdefined'] = 'Benutzerdefiniert:';
+$string['eventassessableuploaded'] = 'A GeoGebra submission has been uploaded:';
---
+++ lang/en/assignsubmission_geogebra.php
@@ -0,0 +1,48 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'assignsubmission_geogebra', language 'en'
+ *
+ * @package        assignsubmission_geogebra
+ * @author         Christoph Stadlbauer <christoph.stadlbauer@geogebra.org>
+ * @copyright  (c) International GeoGebra Institute 2014
+ * @license        http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later,
+ * license of GeoGebra: http://creativecommons.org/licenses/by-nc-nd/3.0/
+ * For commercial use please see: http://www.geogebra.org/license
+ */
+
+$string['allowgeogebrasubmissions'] = 'Enabled';
+$string['default'] = 'Enabled by default';
+$string['default_help'] = 'If set, this submission method will be enabled by default for all new assignments.';
+$string['enabled'] = 'GeoGebra';
+$string['enabled_help'] = 'If enabled, students are able to use GeoGebra for their submission.';
+$string['geogebra'] = 'GeoGebra';
+$string['geogebrafilename'] = 'geogebra.html';
+$string['geogebrasubmission'] = 'Allow GeoGebra submission';
+$string['pluginname'] = 'GeoGebra submissions';
+$string['ggbturl'] = 'GeoGebra template to use for student';
+$string['ggbturl_help'] = 'The "Standard"-Templates are not working at the moment, only user defined works. This file will be used
+as template for the student.';
+$string['ggbtemplates'] = 'Choose a template';
+$string['algebragraphics'] = 'Algebra and Graphics';
+$string['basicgeo'] = 'Basic Geometry';
+$string['geometry'] = 'Geometry';
+$string['spreadsheetgraphics'] = 'Spreadsheet and Graphics';
+$string['casgraphics'] = 'CAS and Graphics';
+$string['graphics3d'] = '3D Graphics';
+$string['userdefined'] = 'User defined:';
+$string['eventassessableuploaded'] = 'Eine GeoGebra Datei wurde abgegeben:';
