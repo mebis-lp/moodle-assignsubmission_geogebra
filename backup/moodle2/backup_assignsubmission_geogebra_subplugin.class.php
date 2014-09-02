6cf68e67ce005182866f92f2fe3e682a3764aa44
christoph.stadlbauer <christoph.stadlbauer@872ce066-4876-4fca-a6fc-23b94bf576d1>
2014-09-02 09:55:09 +0000

    New Directory structure
    git-svn-id: https://dev.geogebra.org/internal/moodle/moodle-assignsubmission_geogebra/trunk@3006 872ce066-4876-4fca-a6fc-23b94bf576d1

---
+++ backup/moodle2/backup_assignsubmission_geogebra_subplugin.class.php
@@ -0,0 +1,68 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * This file contains the class for backup of this submission plugin
+ *
+ * @package        assignsubmission_geogebra
+ * @author         Christoph Stadlbauer <christoph.stadlbauer@geogebra.org>
+ * @copyright  (c) International GeoGebra Institute 2014
+ * @license        http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later,
+ * license of GeoGebra: http://creativecommons.org/licenses/by-nc-nd/3.0/
+ * For commercial use please see: http://www.geogebra.org/license
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Provides the information to backup geogebra submissions
+ *
+ * This just records the submission and format
+ *
+ * @package        assignsubmission_geogebra
+ * @author         Christoph Stadlbauer <christoph.stadlbauer@geogebra.org>
+ * @copyright  (c) International GeoGebra Institute 2014
+ * @license        http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later,
+ * license of GeoGebra: http://creativecommons.org/licenses/by-nc-nd/3.0/
+ * For commercial use please see: http://www.geogebra.org/license
+ */
+class backup_assignsubmission_geogebra_subplugin extends backup_subplugin {
+
+    /**
+     * Returns the subplugin information to attach to submission element
+     *
+     * @return backup_subplugin_element
+     */
+    protected function define_submission_subplugin_structure() {
+
+        // Create XML elements.
+        $subplugin = $this->get_subplugin_element();
+        $subpluginwrapper = new backup_nested_element($this->get_recommended_name());
+        $subpluginelement = new backup_nested_element('submission_geogebra',
+                null,
+                array('ggbparameters', 'ggbviews', 'ggbcodebaseversion', 'submission'));
+
+        // Connect XML elements into the tree.
+        $subplugin->add_child($subpluginwrapper);
+        $subpluginwrapper->add_child($subpluginelement);
+
+        // Set source to populate the data.
+        $subpluginelement->set_source_table('assignsubmission_geogebra',
+                array('submission' => backup::VAR_PARENTID));
+
+        return $subplugin;
+    }
+}
